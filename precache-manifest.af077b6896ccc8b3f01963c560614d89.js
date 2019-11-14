6ac570a03038eaeda251a7182bedea335e3bba65
Arun Saini <sarun6153@gmail.com>
2019-11-14 14:52:58 +0530

    Updates

---
+++ precache-manifest.af077b6896ccc8b3f01963c560614d89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4c872f5fa192c17155de72fef756a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4c16f19896c932bb331",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "1b433e0249c3cbc27395",
+    "url": "/static/css/main.6cadca0a.chunk.css"
+  },
+  {
+    "revision": "f4c16f19896c932bb331",
+    "url": "/static/js/2.7dd5c705.chunk.js"
+  },
+  {
+    "revision": "1b433e0249c3cbc27395",
+    "url": "/static/js/main.b488fd96.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  },
+  {
+    "revision": "4af24fffa29618ec040157a333218ca2",
+    "url": "/static/media/avatar_about.4af24fff.jpg"
+  }
+]);
